0cc94e35412836601bb1de55bc50566df3c1985d
qidcoder <qidcoder@gmail.com>
2023-04-11 10:56:20 -0400

    nested view in a geometryReader to adjust to different screen sizes

--- start/TutorialApp/OvercastView.swift
+++ start/TutorialApp/OvercastView.swift
@@ -3,58 +3,61 @@ import SwiftUI
 // 10..<35
 struct OvercastView: View {
     var body: some View {
-        VStack{
-            //titles
-            HStack{
-                Image(systemName: "square")
-                    .foregroundColor(.orange)
-                    .font(.title3) //you can add font modifiers to images too
-                VStack{
-                    Text("Title 1")
-                    Text("subtitle".uppercased())
-                        .font(.caption)
-                        .foregroundColor(.gray)
-                }
-                .frame(maxWidth: .infinity) //takes up width of device
-                Image(systemName: "square")
-                    .foregroundColor(.orange)
-                    .font(.title3)
-            }
-            //image
-            Rectangle()
-
-            //timestamp
+        GeometryReader { geometry in
             VStack{
-                Divider()
-                    .foregroundColor(.orange)
+                //titles
                 HStack{
-                    Text("35:00")
-                    Text("1:17")
+                    Image(systemName: "square")
+                        .foregroundColor(.orange)
+                        .font(.title3)
+                    VStack{
+                        Text("Title 1")
+                        Text("subtitle".uppercased())
+                            .font(.caption)
+                            .foregroundColor(.gray)
+                    }
+                    .frame(maxWidth: .infinity)
+                    Image(systemName: "square")
+                        .foregroundColor(.orange)
+                        .font(.title3)
                 }
-                .foregroundColor(.gray)
+                //image
+                Rectangle()
 
-            }
+                //timestamp
+                VStack{
+                    Divider()
+                        .foregroundColor(.orange)
+                    HStack{
+                        Text("35:00")
+                        Text("1:17")
+                    }
+                    .foregroundColor(.gray)
 
-            //constrols
-            HStack{
-                Image(systemName: "square")
-                Image(systemName: "square")
-                Image(systemName: "square")
-            }
-            .foregroundColor(.orange)
-            .font(.largeTitle)
-            .scaleEffect(1.5) //can make it even bigger
+                }
 
-            //constrol2
-            HStack{
-                Image(systemName: "square")
-                Image(systemName: "square")
-                Image(systemName: "square")
-                Image(systemName: "square")
-                Image(systemName: "square")
+                //constrols
+                HStack{
+                    Image(systemName: "square")
+                    Image(systemName: "square")
+                    Image(systemName: "square")
+                }
+                .foregroundColor(.orange)
+                .font(.largeTitle)
+                .scaleEffect(1.5)
+
+                //constrol2
+                HStack{
+                    Image(systemName: "square")
+                    Image(systemName: "square")
+                    Image(systemName: "square")
+                    Image(systemName: "square")
+                    Image(systemName: "square")
+                }
+                .foregroundColor(.orange)
             }
-            .foregroundColor(.orange)
         }
+
     }
 }
 
